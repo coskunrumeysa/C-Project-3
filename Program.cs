555eed743efdac2b1c683f9bb24d650109bbef17
coskunrumeysa <70705198+coskunrumeysa@users.noreply.github.com>
2022-05-26 15:28:19 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿//Beni Yaratan Elbet Yolumu Gösterir Şuara78
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("*****Kimlik Bilgileri *****");
+            Console.WriteLine();
+            Console.WriteLine("Rümeysa");
+            Console.WriteLine("Çoşkun");
+            Console.WriteLine("Üsküdar Üniversitesi");
+            Console.Write("İstanbul");
+            Console.WriteLine();
+            Console.Write("*****Kimlik Bilgileri*****");
+            Console.Read();
+        }
+    }
+}
